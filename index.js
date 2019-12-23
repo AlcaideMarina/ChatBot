581275299e03da35a2d689d63305feb3ef6b03fe
AlcaideMarina <marinaalcaidec99@gmail.com>
2019-12-23 11:00:59 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,79 @@
+"use strict";
+const functions = require("firebase-functions");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const BIGQUERY = require("@google-cloud/bigquery");
+const BIGQUERY_CLIENT = new BIGQUERY({
+projectId: "lucid-copilot-262923"
+});
+process.env.DEBUG = "dialogflow:debug";
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
+(request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log(
+    "Dialogflow Request headers: " + JSON.stringify(request.headers)
+  );
+  console.log("Dialogflow Request body: " + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function ticketCollection(agent) {
+    // Capture Parameters from the Current Dialogflow Context
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    const OUTPUT_CONTEXTS = request.body.queryResult.outputContexts;
+    const EMAIL = OUTPUT_CONTEXTS[OUTPUT_CONTEXTS.length - 2].parameters["email.original"];
+    const ISSUE_CATEGORY = OUTPUT_CONTEXTS[OUTPUT_CONTEXTS.length - 1].parameters.category;
+    const ISSUE_TEXT = request.body.queryResult.queryText;
+ 
+
+    // The SQL Query to Run
+    const SQLQUERY = `WITH pred_table AS (SELECT 5 as seniority, "3-Advanced" as experience,
+          @category as category, "Request" as type)
+          SELECT cast(predicted_label as INT64) as predicted_label
+          FROM ML.PREDICT(MODEL helpdesk.prediccion1,  TABLE pred_table)`;
+
+    const OPTIONS = {
+      query: SQLQUERY,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: "US",
+      params: {
+        category: ISSUE_CATEGORY
+      }
+    };
+    return BIGQUERY_CLIENT.query(OPTIONS)
+      .then(results => {
+        //Capture results from the Query
+        console.log(JSON.stringify(results[0]));
+        const QUERY_RESULT = results[0];
+        const ETA_PREDICTION = QUERY_RESULT[0].predicted_label;
+    
+        //Format the Output Message
+        agent.add(EMAIL + ", your ticket has been created. Someone will you contact shortly. " +
+            " The estimated response time is " + ETA_PREDICTION  + " days."
+        );
+        
+        agent.setContext({
+          name: "submitticket-collectname-followup",
+          lifespan: 2
+        });
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  }
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+  intentMap.set("Submit Ticket - Issue Category", ticketCollection);
+  agent.handleRequest(intentMap);
+}
+);
